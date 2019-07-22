c340da758e2166cf1e4d174a0f4dcc46fec2d7c4
tiennv <tiennguyen200597@gmail.com>
2019-07-22 21:57:28 +0700

    setting bootstrap

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+//npm install nodemon rồi chạy nodemon index.js để không cần chạy lại server
 var express = require('express');
 var app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/'));
 
 // index page
-app.get('/', function(req, res) {
+app.get(['/','page-login.html'], function(req, res) {
     res.render('page-login');
 });
-app.get('/page-login.html', function(req, res) {
+app.get('/index.html', function(req, res) {
     res.render('index');
 });
 app.get('/page-register.html', function(req, res) {
